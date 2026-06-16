b9e7e194f262992db78359160932fb6ffdcac4e0
Aashirzahid1 <148004277+Aashirzahid1@users.noreply.github.com>
2026-06-16 20:42:56 +0500

    Initial python functions practice

---
+++ hello.py
@@ -0,0 +1,18 @@
+def greet(name):
+    print("Hello", name)
+
+greet("Aashir")
+
+
+def add(a,b):
+    return a+b
+print(add(3,5))
+
+def is_even(num):
+    if (num%2)==0:
+        return True
+    else:
+        return False
+
+print(is_even(4))
+print(is_even(5))
