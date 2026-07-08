a727cb170e0c5078ab44b81fc54563d4a19e3cb2
Aashirzahid1 <148004277+Aashirzahid1@users.noreply.github.com>
2026-07-08 19:46:09 +0500

    Library Management system Using Classes

--- hello.py
+++ hello.py
@@ -1,74 +1,100 @@
-students=[]
-add=[]
-while True:
-    
-    print("""
-         1. Add Students
-         2. Veiw Students
-         3. Update Students
-         4. Remove Students
-         5. Show Highest Grade
-         6. Show Lowest Grade
-         7. Show Average Grade
-          8. Exit
-          9. search student
-           """)
-    
+class Book:
+    def __init__(self,book_name,author,isbn):
+         self.book_name=book_name
+         self.author=author
+         self.isbn=isbn
+         self.available=True
 
-    num=int(input("Enter the choice"))
+    def display_info(self):
+         print(f"Book Name: {self.book_name}")
+         print(f"Author: {self.author}")
+         print(f"ISBN: {self.isbn}")
+         print(f"Availability: {"Available" if self.available else "Borrowed"}")
 
-    if num==1:
-            name=input("Enter the student name")
-            add.append(name)
-            grade=int(input("Enter his Grade"))
-            add.append(grade)
-            students.append(add.copy())
-            print(students)
-            add.clear()
-            
-    elif num==2:
-          for stu in students:
-                print(stu)
-                print(stu[0]," - ",stu[1])
-    elif num==3:
-          for stu,index in enumerate(students):
-                print(stu,index)
-          upgrade=int(input("Enter the index of student whose grade you want to be updated"))
-          new_grade=int(input("enter new grade"))
-          if 0<=upgrade<=len(students)-1:
-                students[upgrade][1]=new_grade
-    elif num==4:
-          for student,index in enumerate(students):
-                print(students,index)
-          remove=int(input("enter the student index you want to remove"))
-          students.pop(remove)
-    elif num==5:
-          highest_grade=students[0][1]
-          print(highest_grade)
-          for student in students:
-                      if student[1]>highest_grade:
-                            highest_grade=student[1]
-          print("The highest grade is:",highest_grade)
-    elif num==6:
-          lowest_grade=students[0][1]
-          for student in students:
-                      if student[1]<lowest_grade:
-                            lowest_grade=student[1]
-          print("the lowest grade is: ",lowest_grade)  
-    elif num==7:
-          total=0
-          count=0
-          for student in students:
-                      count+=1
-                      total+=student[1]
-          print(f"Average: {total/count}")
-    elif num==8:
-          print("Exiting the student portal")
-          break
-    elif num==9:
-           search=input("Enter student name")
-           for student in students:
-                  if student[0]==search:
-                         print(student[0]," - ",student[1])
+class Member:
+     def __init__(self,member_name,member_id):
+          self.member_name=member_name
+          self.member_id=member_id
+          self.borrowed_books=[]
+
+
+     def display_borrowed_books(self):
+          for book in self.borrowed_books:
+               print(f"Books: {book.display_info()}")
+
+class Library:
+     def __init__(self):
+          self.books=[]
+          self.members=[]
+
+     def add_book(self, book):
+          self.books.append(book)
+     
+     def add_member(self,member):
+          self.members.append(member)
+
+     def borrow_book(self,member,book):
+         
+        if member in self.members: 
+             if book in self.books:  
+               if book.available==True:
+                    member.borrowed_books.append(book)
+                    book.available=False
+               elif book in member.borrowed_books:
+                  print("You HAve Already borrowed it")
+               else:
+                    print("The book is already borrowed")
+             else:
+                  print("Book not Found")
+        else:
+             print("Member not found")
+
+
+     def return_book(self,member,book):
+             if member in self.members: 
+                  if book in self.books:
+                       if book.available:
+                            print("Not borrowed Yet")
+                       else:
+                            if book in member.borrowed_books:
+                                 num=member.borrowed_books.index(book)
+                                 member.borrowed_books.pop(num)
+                                 book.available=True
+                            else:
+                                 print("Not borrowed by this member")
+
                   else:
-                         print("student not found")
+                       print("book not found")
+             else:
+                  print("Member not registered")
+     def search_book(self,book_name):
+          for book in self.books:
+               if book.book_name == book_name:
+                    return book
+          return None
+     
+     def display_books(self):
+        if len(self.books) == 0:
+          print("No Book is Available")
+        else:
+          for book in self.books:
+               print(book.display_info())
+
+     def remove_book(self,book):
+          if book in self.books:
+               if book.available:
+                    num=self.books.index(book)
+                    self.books.pop(num)
+               else:
+                   print("The book is already issued wait for return")
+          else:
+               print("The book is not registered")
+
+
+
+book1=Book("Clean Code", "Roberto C. Martin", "12345")
+member1=Member("Aashir","bs-1")
+library = Library()
+
+library.add_book(book1)
+library.add_member(member1)
